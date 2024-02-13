9a1ad85410d9386a0e259cdd3ed45ae2075c0465
Zar Ni Tun <zarnitunkt@gmail.com>
2024-02-13 21:44:45 +0630

    add word count and random class methods

--- wordfinder.py
+++ wordfinder.py
@@ -1,8 +1,43 @@
 """Word Finder: finds random words from a dictionary."""
 
-file = open("words.txt","r")
-class WordFinder:
-    for line in file:
-        print(line)
+import random
 
-print(file)
+
+
+
+class WordFinder:
+    '''
+    >>> wf = WordFinder("/Users/student/words.txt")
+    3 words read
+
+    >>> wf.random()
+    'cat'
+
+    >>> wf.random()
+    'cat'
+
+    >>> wf.random()
+    'porcupine'
+
+    >>> wf.random()
+    'dog'
+    '''
+    
+    def __init__(self,path):
+        file = open(path)
+        self.words = self.parse(file)
+        print(f"{len(self.words)} words read")
+        
+         
+    def parse(self, dict_file):
+        """Parse dict_file -> list of words."""
+
+        return [w.strip() for w in dict_file]
+
+    def random(self):
+        """Return random word."""
+
+        return random.choice(self.words)
+
+wf = WordFinder("words.txt")
+print(wf.random())
